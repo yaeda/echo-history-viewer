9cc85e993de9b7a861084376166e250f94a25b2d
Takeshi YAEDA <t.yaeda@gmail.com>
2015-03-02 00:52:01 +0900

    Add build and deploy task

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp        = require('gulp');
 var browserSync = require('browser-sync');
+var useref      = require('gulp-useref');
+var deploy      = require('gulp-gh-pages');
+
+gulp.task('build', function () {
+  var assets = useref.assets();
+
+  return gulp.src('app/*.html')
+    .pipe(assets)
+    .pipe(assets.restore())
+    .pipe(useref())
+    .pipe(gulp.dest('dist'));
+})
+
+gulp.task('deploy', ['build'], function () {
+  return gulp.src('dist/**/*')
+    .pipe(deploy());
+});
 
 gulp.task('browser-sync', function () {
   browserSync({
